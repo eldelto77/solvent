bd8702a3345e53b3e857ad95e8f4a574290dd717
eldelto <eldelto77@gmail.com>
2020-07-01 20:06:53 +0200

    Improve add item button

--- react-client/src/solvent/render/AddItemBar.js
+++ react-client/src/solvent/render/AddItemBar.js
@@ -4,19 +4,23 @@ import { ReactComponent as Plus } from '../../icons/plus.svg'
 import { ReactComponent as PlusCircle } from '../../icons/plus-circle.svg'
 
 export default function AddItemBar(props) {
+
+  const inputField = React.createRef();
+
   return (
-    <form className="AddItemBar" onSubmit={props.onSubmit}>
-      <button className="AddItemBarLogo" type="button" disabled="true">+</button>
+    <form className="AddItemBar" onSubmit={props.onSubmit} onClick={() => inputField.current.focus()}>
+      <button className="AddItemBarButton" type="submit" value="" disabled={props.disabled}>
+        {props.disabled ? <Plus /> : <PlusCircle />}
+      </ button>
       <input
+        ref={inputField}
         className="AddItemBarTitle"
         type="text"
         value={props.value}
         placeholder="New item"
         onChange={props.onChange}
       />
-      <button className="AddItemBarButton" type="submit" value="" disabled={props.disabled}>
-        {props.disabled ? <Plus /> : <PlusCircle />}
-      </ button>
+      <button className="AddItemBarLogo" type="button" disabled="true">+</button>
     </form>
   );
 }
